3057a3de6efd8f6c82fd1c99e8ceca20133e3969
madrail <neil.sandberg@gmail.com>
2011-09-06 18:43:03 -0500

    Made a Rails project, stuffed it in folder called thrones.

---
+++ thrones/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thrones::Application.config.secret_token = '52a12a80775eb8a08c0612bd352f03a10827b656abcae0e445cad706dff03319406f3df1aff7b5d2b05f98e6e9f068a3f610ddd9a5a8edfbebadd568f0559f0e'
