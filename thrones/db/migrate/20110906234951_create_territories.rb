14551c0c63dd5f3270a35d4a4a789ab74ec2965b
Alex Brown <alexpbrown9@gmail.com>
2011-09-08 21:42:31 -0400

    'type' is a reserved word...

--- thrones/db/migrate/20110906234951_create_territories.rb
+++ thrones/db/migrate/20110906234951_create_territories.rb
@@ -2,7 +2,7 @@ class CreateTerritories < ActiveRecord::Migration
   def change
     create_table :territories do |t|
       t.string :name #name of the territory
-      t.integer :type #whether it is land (1), sea (2), or port (3)
+      t.integer :territory_type #whether it is land (1), sea (2), or port (3)
       t.integer :crowns #number of crowns on the territory
       t.integer :supply #number of barrels on the territory
       t.integer :mustering_points #if a city or fortication, number of mustering points provided
