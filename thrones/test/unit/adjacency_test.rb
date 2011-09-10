159fc1ed5ebd9ba036d42eceed57fe845d70e466
Alex Brown <alexpbrown9@gmail.com>
2011-09-10 17:15:32 -0400

    FINE NEIL THE FIXTURES HAVE ACCURATE NAMES

--- thrones/test/unit/adjacency_test.rb
+++ thrones/test/unit/adjacency_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class AdjacencyTest < ActiveSupport::TestCase
   test "Sunspear is adjacent to Salt Shore" do
-    assert territories(:one).adjacents.include?(territories(:two))
-    assert territories(:two).adjacents.include?(territories(:one))
+    assert territories(:sunspear).adjacents.include?(territories(:salt_shore))
+    assert territories(:salt_shore).adjacents.include?(territories(:sunspear))
   end
 
   test "Lannisport is NOT adjacent to Sunspear" do
-    assert !territories(:four).adjacents.include?(territories(:one))
+    assert !territories(:lannisport).adjacents.include?(territories(:sunspear))
   end
 end
