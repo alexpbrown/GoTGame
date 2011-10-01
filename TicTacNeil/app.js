1fef42826cbf4880feb9860bb0ff210b2a48866a
madrail <neil.sandberg@gmail.com>
2011-10-01 13:43:07 -0500

    implemented linking sessions to sockets through a handshake

--- TicTacNeil/app.js
+++ TicTacNeil/app.js
@@ -4,16 +4,12 @@
  */
 
 var express = require('express');
-var rooms = [];
+var users = {};
+var sockets = {};
 var app = module.exports = express.createServer();
-var io = require('socket.io').listen(9002);
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+var io = require('socket.io');
+var MemoryStore = express.session.MemoryStore
+var sessionStore = new MemoryStore();
 
 function Room() {
   this.occupants = [];
@@ -35,10 +31,13 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
+  app.use(express.session({ store: sessionStore, secret: 'your secret here', key: 'express.sid' }));
   app.use(express.compiler({ src: __dirname + '/public', enable: ['sass'] }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(function (req, res) {
+    res.end('<h2>Hello, your session id is ' + req.sessionID + '</h2>');
+  });
 });
 
 app.configure('development', function(){
@@ -87,3 +86,54 @@ app.post('/login', function(req, res){
 
 app.listen(9001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+var sio = io.listen(app);
+var parseCookie = require('connect').utils.parseCookie;
+var Session = require('connect').middleware.session.Session;
+
+sio.set('authorization', function (data, accept) {
+    if (data.headers.cookie) {
+        data.cookie = parseCookie(data.headers.cookie);
+        data.sessionID = data.cookie['express.sid'];
+        // save the session store to the data object 
+        // (as required by the Session constructor)
+        data.sessionStore = sessionStore;
+        sessionStore.get(data.sessionID, function (err, session) {
+            if (err) {
+                accept(err.message, false);
+            } else {
+                // create a session object, passing data as request and our
+                // just acquired session data
+                data.session = new Session(data, session);
+                accept(null, true);
+            }
+        });
+    } else {
+       return accept('No cookie transmitted.', false);
+    }
+});
+
+sio.sockets.on('connection', function (socket) {
+    var hs = socket.handshake;
+    console.log('A socket with sessionID ' + hs.sessionID 
+        + ' connected!');
+    // setup an inteval that will keep our session fresh
+    var intervalID = setInterval(function () {
+        // reload the session (just in case something changed,
+        // we don't want to override anything, but the age)
+        // reloading will also ensure we keep an up2date copy
+        // of the session with our connection.
+        hs.session.reload( function () { 
+            // "touch" it (resetting maxAge and lastAccess)
+            // and save it back again.
+            hs.session.touch().save();
+        });
+    }, 60 * 1000);
+    socket.on('disconnect', function () {
+        console.log('A socket with sessionID ' + hs.sessionID 
+            + ' disconnected!');
+        // clear the socket interval to stop refreshing the session
+        clearInterval(intervalID);
+    });
+ 
+});
